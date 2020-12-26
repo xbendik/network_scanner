78899e22d4bd307fbdc63f44e8c776d1ec94e9fc
Tom <benda.tomas@gmail.com>
2020-12-26 03:45:12 +0100

    nothing..

--- network_scanner.py
+++ network_scanner.py
@@ -1,16 +1,16 @@
 #!usr/bin/env python
 
-import scapy.all as scapy
+#import scapy.all as scapy
+from scapy.layers import all as scapy
+
 
 def scan(ip):
-    scapy.arping(ip)
+    #scapy.arping(ip)
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
+    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered_list.summary())
 
-    print(arp_request_broadcast.summary())
-
-    arp_request_broadcast.show()
-
-scan("192.168.108.1/24")
+scan("192.168.1.1/24")
 
