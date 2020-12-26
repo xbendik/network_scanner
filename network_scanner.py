6a7e6243039b765f5290718b05514825612f7433
Tom <benda.tomas@gmail.com>
2020-12-26 15:07:18 +0100

    cleaned

--- network_scanner.py
+++ network_scanner.py
@@ -5,12 +5,21 @@ from scapy.layers import all as scapy
 
 
 def scan(ip):
-    #scapy.arping(ip)
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered_list.summary())
+    answered_list = scapy.srp(arp_request_broadcast, timeout=3, verbose=False)[0]
 
-scan("192.168.1.1/24")
+    clients_list = []
+    for row in answered_list:
+        clients_row = {"ip": row[1].psrc, "mac": row[1].hwsrc}
+        clients_list.append(clients_row)
+    return clients_list
 
+def print_result(results_list):
+    print("IP\t\t\tMAC address\n-----------------------------------------")
+    for row in results_list:
+        print(row["ip"] + "\t\t" + row["mac"])
+
+scan_result = scan("192.168.1.1/24")
+print_result(scan_result)
